5500f63b76ff0c18423f7b165313e8826a515e63
Danesh M <daneshm90@gmail.com>
2012-11-29 22:26:21 -0500

    Camera : allow camera to use power key as shutter
    Provides a way for an app to take control of the power key.
    Used by the camera to make the power key control the shutter.
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a
    (cherry picked from commit 15661444ae1faea831218f0c936b756de2f0698b)
    
    Conflicts:
    	core/java/android/view/WindowManager.java
    	core/res/res/values/cm_strings.xml

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5055,6 +5055,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                if ((mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
+                        & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY) != 0){
+                    return result;
+                }
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
                     boolean panic = mImmersiveModeConfirmation.onPowerKeyDown(interactive,

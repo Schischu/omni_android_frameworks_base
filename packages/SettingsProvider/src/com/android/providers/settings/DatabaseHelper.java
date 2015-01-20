f473a5260135c1db7593c4dc875e5966a3d25cce
Adnan Begovic <adnan@cyngn.com>
2015-01-19 18:16:05 -0800

    Migrate QS_TILES to Settings.Secure
    Change-Id: I42783f574d7e050a04e960c91883f2aefc8ab7d6
    
    Conflicts:
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -70,7 +70,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 113;
+
+    private static final int DATABASE_VERSION = 118;
+
 
     private Context mContext;
     private int mUserHandle;
@@ -1825,6 +1827,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 if (stmt != null) stmt.close();
             }
             upgradeVersion = 113;
+        }
+
+
+        if (upgradeVersion < 118) {
+            String[] qsTiles = new String[] {
+                    Settings.Secure.QS_TILES,
+                    Settings.Secure.QS_USE_MAIN_TILES
+            };
+
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
+                    qsTiles, true);
+            upgradeVersion = 118;
         }
 
         // *** Remember to update DATABASE_VERSION above!
